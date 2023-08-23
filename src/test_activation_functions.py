692a2534bc6d420534a5bc9da3e9eefb7c60cfdc
Patrik Keinonen <patrik@wirmark.com>
2023-08-23 15:00:50 +0300

    korjattiin relu testit. lisättiin leaky relu ja tanh testit

--- src/test_activation_functions.py
+++ src/test_activation_functions.py
@@ -22,11 +22,28 @@ def test_relu():
 
 def test_relu_derivative():
     relu = ReLU()
-    result = relu.activation(test_set)
+    result = relu.derivative(test_set)
     for i in range(0, 4):
         assert result[i] == 0
     for i in range(4, len(test_set)):
+        assert result[i] == 1
+
+
+def test_leaky_relu():
+    relu = LeakyReLU()
+    result = relu.activation(test_set)
+    for i in range(0, 4):
+        assert result[i] == (test_set[i] * 0.01)
+    for i in range(4, len(test_set)):
         assert result[i] == test_set[i]
+
+def test_leaky_relu_derivative():
+    relu = LeakyReLU()
+    result = relu.derivative(test_set)
+    for i in range(0, 4):
+        assert result[i] == 0.01
+    for i in range(4, len(test_set)):
+        assert result[i] == 1
 
 
 def test_sigmoid():
@@ -45,3 +62,20 @@ def test_sigmoid_derivative():
     sigmoid = Sigmoid()
     result = sigmoid.derivative(np.array([0]))
     assertAlmostEqual(result[0], 0.25)
+   
+def test_tanh():
+    tanh = Tanh()
+    result = tanh.activation(np.array([0, 1, -1, 100, -100]))
+    # Sigmoid-funktion ominaisuus: 0 on keskellä
+    assertAlmostEqual(result[0], 0)
+    assertAlmostEqual(result[1], 0.7615941559557649)
+    assertAlmostEqual(result[2], -0.7615941559557649)
+    # todella suuret arvot ~1
+    assertAlmostEqual(result[3], 1)
+    # todella pienet arvot ~0
+    assertAlmostEqual(result[4], -1)  
+    
+def test_tanh_derivative():
+    tanh = Tanh()
+    result = tanh.derivative(np.array([0]))
+    assertAlmostEqual(result[0], 1)
